658959142a0797883b5de56da5d681f0f8aea303
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-12-18 00:08:18 +0100

    zad1 - class File read method test improved

--- tests/test_zad1.py
+++ tests/test_zad1.py
@@ -7,9 +7,13 @@ class TestFile(unittest.TestCase):
 
     def test_read(self):
         file = "Lorem ipsum dolor sit amet"
-        path = "file.txt"
-        with patch('builtins.open', mock_open(read_data=file)):
+        path = "./mock/file.txt"
+        open_mock = mock_open(read_data=file)
+
+        with patch('builtins.open', open_mock):
             self.assertEqual(["Lorem ipsum dolor sit amet"], self.file.read(path))
+
+        open_mock.assert_called_once_with(path, 'r')
 
     def setUp(self):
         self.file = File()
