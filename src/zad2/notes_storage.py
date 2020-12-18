3865091ecc5edbd60428595dc425303f4ce69372
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-12-18 01:02:19 +0100

    zad1 - class NoteStorage methods implemented

---
+++ src/zad2/notes_storage.py
@@ -0,0 +1,13 @@
+class NotesStorage:
+
+    def __init__(self):
+        self.notes = []
+
+    def add(self, note):
+        pass
+
+    def clear(self):
+        pass
+
+    def get_all_notes_of(self, name):
+        pass
