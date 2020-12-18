77ba8399a3363dd87855640f33df226072aff290
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2020-12-18 01:03:33 +0100

    zad2 - class NotesService methods implemented

---
+++ src/zad2/notes_service.py
@@ -0,0 +1,24 @@
+from notes_storage import NotesStorage
+
+
+class NotesService:
+
+    def __init__(self):
+        self.notes = NotesStorage()
+
+    def add(self, note):
+        if not hasattr(note, 'name') or not hasattr(note, 'note'):
+            raise TypeError("Input has to be object of Note class")
+
+        self.notes.add(note)
+
+    def averageOf(self, name):
+        allNotes = self.notes.get_all_notes_of(name)
+
+        if not allNotes:
+            raise Exception("This student has no grades!")
+
+        return sum(x.note for x in allNotes) / len(allNotes)
+
+    def clear(self):
+        self.notes.clear()
